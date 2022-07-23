61fc7c3650fde7c63eb0f62d73cde4354d3d2002
Joaquín Weiss <joaco.w93@gmail.com>
2022-07-23 20:59:57 -0300

    added styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
-import axios from 'axios';
+import React from "react"; // use state missing
+//import axios from 'axios';
 
 function App() {
 
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=a7fd1a650c73cda0a269de66910d8acb'
+  //const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=a7fd1a650c73cda0a269de66910d8acb'
 
 
   return (
